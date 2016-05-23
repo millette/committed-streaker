07f070a59e11096203e115f2c09be2e8d304fcb8
Robin Millette <robin@millette.info>
2016-05-23 08:54:16 -0400

    Cleanup.

--- test/utils.js
+++ test/utils.js
@@ -4,7 +4,7 @@ import test from 'ava'
 import fn from '../lib/utils'
 
 test('utils sessionSecret', t => {
-  t.plan(4)
+  t.plan(6)
 
   const a1 = fn.sessionSecret('a')
   const a2 = fn.sessionSecret('a')
@@ -14,4 +14,6 @@ test('utils sessionSecret', t => {
   t.not(a1, b)
   t.is(a1.length, 44)
   t.is(b.length, 44)
+  t.is(a1.slice(-1), '=')
+  t.is(b.slice(-1), '=')
 })
