767ef2f15d016ef37d925938b953bdd55afcd81d
Robin Millette <robin@millette.info>
2016-07-11 09:23:33 -0400

    Batch update everything every 24h; removed extraneous dependency requires.

--- cli.js
+++ cli.js
@@ -2,8 +2,61 @@
 
 'use strict'
 
+// npm
+const debug = require('debug')('yummy')
+
 // self
 const utils = require('./lib/utils')
 
-utils.dailyUpdates()
-setInterval(utils.dailyUpdates, utils.dayUnit / 24)
+/*
+const dailyUpdates = () => utils.userDB.view('app', 'probs', (err, body) => {
+  if (err) { return debug('dailyUpdates error: %s', err) }
+  let data = shuffle(body.rows.filter((x) => Math.floor(1 + Math.random() * 1000) < 10 * x.key))
+
+  data = data.slice(0, Math.floor(data.length / 20))
+  const delay = Math.max(0.3, 30000 / data.length) // spread over 30s min.
+
+  data.forEach((r, k) => {
+    debug('setup contrib updates for %s', r.id)
+    setTimeout((name) => {
+      debug('contrib updates ready for %s', name)
+      utils.refresh(name)
+    }, k * delay, utils.couchUserToName(r))
+  })
+
+  setTimeout((name) => {
+    utils.cdb.db.compact('u2', (err, body) => {
+      if (err) { return debug('Compacting Error: %s', err) }
+      debug('(%s) Compacting OK: %s', new Date().toISOString(), JSON.stringify(body))
+    })
+  }, 60000 + data.length * delay)
+
+  debug('(%s) nItems: %s; delay: %s', new Date().toISOString(), data.length, delay)
+})
+*/
+
+const dailyUpdates = () => utils.userDB.view('app', 'probs', (err, body) => {
+  if (err) { return debug('dailyUpdates error: %s', err) }
+  const data = body.rows.reverse()
+  const delay = 500
+
+  data.forEach((r, k) => {
+    debug('setup contrib updates for %s', r.id)
+    setTimeout((name) => {
+      debug('contrib updates ready for %s', name)
+      utils.refresh(name)
+    }, k * delay, utils.couchUserToName(r))
+  })
+
+  setTimeout((name) => {
+    utils.cdb.db.compact('u2', (err, body) => {
+      if (err) { return debug('Compacting Error: %s', err) }
+      debug('(%s) Compacting OK: %s', new Date().toISOString(), JSON.stringify(body))
+    })
+  }, 60000 + data.length * delay)
+
+  debug('(%s) nItems: %s; delay: %s', new Date().toISOString(), data.length, delay)
+})
+
+dailyUpdates()
+setInterval(dailyUpdates, utils.dayUnit)
