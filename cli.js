0a7398db5e1f6e3f0c11ff384d803c497083efd6
Robin Millette <robin@millette.info>
2016-07-14 04:56:15 -0400

    Removed dead code.

--- cli.js
+++ cli.js
@@ -9,44 +9,17 @@ const debug = require('debug')('yummy')
 // self
 const utils = require('./lib/utils')
 
-/*
-const dailyUpdates = () => utils.userDB.view('app', 'probs', (err, body) => {
-  if (err) { return debug('dailyUpdates error: %s', err) }
-  let data = shuffle(body.rows.filter((x) => Math.floor(1 + Math.random() * 1000) < 10 * x.key))
-
-  data = data.slice(0, Math.floor(data.length / 20))
-  const delay = Math.max(0.3, 30000 / data.length) // spread over 30s min.
-
-  data.forEach((r, k) => {
-    debug('setup contrib updates for %s', r.id)
-    setTimeout((name) => {
-      debug('contrib updates ready for %s', name)
-      utils.refresh(name)
-    }, k * delay, utils.couchUserToName(r))
-  })
-
-  setTimeout((name) => {
-    utils.cdb.db.compact('u2', (err, body) => {
-      if (err) { return debug('Compacting Error: %s', err) }
-      debug('(%s) Compacting OK: %s', new Date().toISOString(), JSON.stringify(body))
-    })
-  }, 60000 + data.length * delay)
-
-  debug('(%s) nItems: %s; delay: %s', new Date().toISOString(), data.length, delay)
-})
-*/
-
 const dailyUpdates = (options) => utils.userDB.view(
   'app', 'probs', options || { descending: true },
   (err, body) => {
     if (err) { return debug('dailyUpdates error: %s', err) }
-    const data = body.rows
-    const delay = 400
+    const data = sample(body.rows, Math.floor(data.length / 2))
+    const delay = 600
 
-    sample(data, Math.floor(data.length / 2)).forEach((r, k) => {
+    data.forEach((r, k) => {
       debug('setup contrib updates for %s', r.id)
       setTimeout((name) => {
-        debug('contrib updates ready for %s', name)
+        debug('updating contrib for %s', name)
         utils.refresh(name)
       }, k * delay, utils.couchUserToName(r))
     })
@@ -68,5 +41,5 @@ const dailyZero = dailyUpdates.bind(null, { endkey: 75 }) // 4000
 
 dailySome()
 setTimeout(() => setInterval(dailySome, utils.dayUnit / 7), utils.dayUnit / 7)
-setInterval(dailyWorked, utils.dayUnit / 24)
+setInterval(dailyWorked, utils.dayUnit / 20)
 setInterval(dailyZero, utils.dayUnit)
